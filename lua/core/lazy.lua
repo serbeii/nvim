bdcd64334fb8b442e80aa72ff5775c084bd7e96f
serbeii <cem.serimer@tedu.edu.tr>
2023-12-13 01:49:19 +0300

    fixed typo

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -4,7 +4,7 @@ if not vim.loop.fs_stat(lazypath) then
     "git",
     "clone",
     "--filter=blob:none",
- builtin.find_files,    "https://github.com/folke/lazy.nvim.git",
+    "https://github.com/folke/lazy.nvim.git",
     "--branch=stable", -- latest stable release
     lazypath,
   })
