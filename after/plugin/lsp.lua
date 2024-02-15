bb30976042f610adb905dfeeafd550d06c907dbe
serbeii <cem.serimer@tedu.edu.tr>
2024-02-15 16:12:20 +0300

    refactored linting config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -38,33 +38,6 @@ require('mason-lspconfig').setup({
     }
 })
 
-require('mason-tool-installer').setup({
-    ensure_installed = {
-        'sonarlint-language-server'
-    }
-})
-
-require('sonarlint').setup({
-            server = {
-                cmd = {
-                    'sonarlint-language-server',
-                    -- Ensure that sonarlint-language-server uses stdio channel
-                    '-stdio',
-                    '-analyzers',
-                    -- paths to the analyzers you need, using those for python and java in this example
-                    vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarpython.jar"),
-                    vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarcfamily.jar"),
-                    vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjava.jar"),
-                }
-            },
-            filetypes = {
-                -- Tested and working
-                'python',
-                'cpp',
-                'java',
-            }
-        })
-
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
