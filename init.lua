8ff75dd698f901080d1c7c95d0590e8d5a12c74e
Cem Serimer <cdserimer@icloud.com>
2025-02-17 09:46:05 +0300

    removed problematic auto-update

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
 require("core")
-local dir = vim.fn.stdpath('config').."/.git"
-print("updating config from the repository")
-local result = vim.fn.system({
-    "git",
-    "--git-dir="..dir,
-    "pull",
-})
-if not result == 0 or not result:match("Already up to date%.") then
-    print("update failed error code:" .. result)
-end
+--local dir = vim.fn.stdpath('config').."/.git"
+--print("updating config from the repository")
+--local result = vim.fn.system({
+--    "git",
+--    "--git-dir="..dir,
+--    "pull",
+--})
+--if not result == 0 or not result:match("Already up to date%.") then
+--    print("update failed error code:" .. result)
+--end
