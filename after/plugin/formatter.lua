e2b151502b9716c3251eb5639775b9d121af1d3f
Cem Serimer <cdserimer@icloud.com>
2025-03-05 01:06:29 +0300

    Updated formatting, formatted existing files.

--- after/plugin/formatter.lua
+++ after/plugin/formatter.lua
@@ -1,117 +1,189 @@
-local formatter = require('formatter')
+local formatter = require ('formatter')
 
-formatter.setup({
-    filetype = {
-        javascript = {
-            function()
-                return {
-                    exe = "prettier",
-                    args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0), "--single-quote", "--print-width=80", "--tab-width=4" },
-                    stdin = true
-                }
-            end
-        },
-        typescript = {
-            function()
-                return {
-                    exe = "prettier",
-                    args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0), "--single-quote", "--print-width=80", "--tab-width=4" },
-                    stdin = true
-                }
-            end
-        },
-        vue = {
-            function()
-                return {
-                    exe = "prettier",
-                    args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0), "--single-quote", "--print-width=80", "--tab-width=4" },
-                    stdin = true
-                }
-            end
-        },
-        html = {
-            function()
-                return {
-                    exe = "prettier",
-                    args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0), "--single-quote", "--print-width=80", "--tab-width=4" },
-                    stdin = true
-                }
-            end
-        },
-        c = {
-            function()
-                return {
-                    exe = "clang-format",
-                    args = { "--style=\"{BasedOnStyle: Google, IndentWidth: 4}\"", "--assume-filename=" .. vim.api.nvim_buf_get_name(0) },
-                    stdin = true,
-                    cwd = vim.fn.expand('%:p:h'), -- Run clang-format in the directory of the current file
-                }
-            end
-        },
-        cpp = {
-            function()
-                return {
-                    exe = "clang-format",
-                    args = { "--style=Google", "--assume-filename=" .. vim.api.nvim_buf_get_name(0) },
-                    stdin = true,
-                    cwd = vim.fn.expand('%:p:h') -- Run clang-format in the directory of the current file
-                }
-            end
-        },
-        python = {
-            function()
-                return {
-                    exe = "autopep8",
-                    args = { "--indent-size=4", "--max-line-length=80" },
-                    stdin = true
-                }
-            end
-        },
-        markdown = {
-            function()
-                return {
-                    exe = "prettier",
-                    args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0), "--single-quote", "--print-width=80", "--tab-width=4" },
-                    stdin = true
-                }
-            end
-        },
-        yaml = {
-            function()
-                return {
-                    exe = "prettier",
-                    args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0), "--single-quote", "--print-width=80", "--tab-width=4" },
-                    stdin = true
-                }
-            end
-        },
-        json = {
-            function()
-                return {
-                    exe = "prettier",
-                    args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0), "--single-quote", "--print-width=80", "--tab-width=4" },
-                    stdin = true
-                }
-            end
-        },
-        Dockerfile = {
-            function()
-                return {
-                    exe = "prettier",
-                    args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0), "--single-quote", "--print-width=80", "--tab-width=4" },
-                    stdin = true
-                }
-            end
-        },
-        --        lua = {
-        --            function()
-        --                return {
-        --                    exe = "luaformatter",
-        --                    args = { "--indent-width=4", "--column-limit=80" },
-        --                    stdin = true
-        --                }
-        --            end
-        --        },
-    },
-    logging = false -- Disable logging for faster performance
+formatter.setup ({
+	filetype = {
+		javascript = {
+			function ()
+				return {
+					exe = 'prettier',
+					args = {
+						'--stdin-filepath',
+						vim.api.nvim_buf_get_name (0),
+						'--single-quote',
+						'--print-width=80',
+						'--tab-width=4',
+					},
+					stdin = true,
+				}
+			end,
+		},
+		typescript = {
+			function ()
+				return {
+					exe = 'prettier',
+					args = {
+						'--stdin-filepath',
+						vim.api.nvim_buf_get_name (0),
+						'--single-quote',
+						'--print-width=80',
+						'--tab-width=4',
+					},
+					stdin = true,
+				}
+			end,
+		},
+		vue = {
+			function ()
+				return {
+					exe = 'prettier',
+					args = {
+						'--stdin-filepath',
+						vim.api.nvim_buf_get_name (0),
+						'--single-quote',
+						'--print-width=80',
+						'--tab-width=4',
+					},
+					stdin = true,
+				}
+			end,
+		},
+		html = {
+			function ()
+				return {
+					exe = 'prettier',
+					args = {
+						'--stdin-filepath',
+						vim.api.nvim_buf_get_name (0),
+						'--single-quote',
+						'--print-width=80',
+						'--tab-width=4',
+					},
+					stdin = true,
+				}
+			end,
+		},
+		c = {
+			function ()
+				return {
+					exe = 'clang-format',
+					args = {
+						'--style="{BasedOnStyle: Google, IndentWidth: 4}"',
+						'--assume-filename=' .. vim.api.nvim_buf_get_name (0),
+					},
+					stdin = true,
+					cwd = vim.fn.expand ('%:p:h'),
+				}
+			end,
+		},
+		cpp = {
+			function ()
+				return {
+					exe = 'clang-format',
+					args = {
+						'--style="{BasedOnStyle: Google, IndentWidth: 4}"',
+						'--assume-filename=' .. vim.api.nvim_buf_get_name (0),
+					},
+					stdin = true,
+					cwd = vim.fn.expand ('%:p:h'),
+				}
+			end,
+		},
+		python = {
+			function ()
+				return {
+					exe = 'autopep8',
+					args = {
+						'--indent-size=4',
+						'--max-line-length=80',
+						vim.api.nvim_buf_get_name (0),
+					},
+					stdin = true,
+				}
+			end,
+		},
+		markdown = {
+			function ()
+				return {
+					exe = 'prettier',
+					args = {
+						'--stdin-filepath',
+						vim.api.nvim_buf_get_name (0),
+						'--single-quote',
+						'--print-width=80',
+						'--tab-width=4',
+					},
+					stdin = true,
+				}
+			end,
+		},
+		yaml = {
+			function ()
+				return {
+					exe = 'prettier',
+					args = {
+						'--stdin-filepath',
+						vim.api.nvim_buf_get_name (0),
+						'--single-quote',
+						'--print-width=80',
+						'--tab-width=4',
+					},
+					stdin = true,
+				}
+			end,
+		},
+		json = {
+			function ()
+				return {
+					exe = 'prettier',
+					args = {
+						'--stdin-filepath',
+						vim.api.nvim_buf_get_name (0),
+						'--single-quote',
+						'--print-width=80',
+						'--tab-width=4',
+					},
+					stdin = true,
+				}
+			end,
+		},
+		Dockerfile = {
+			function ()
+				return {
+					exe = 'prettier',
+					args = {
+						'--stdin-filepath',
+						vim.api.nvim_buf_get_name (0),
+						'--single-quote',
+						'--print-width=80',
+						'--tab-width=4',
+					},
+					stdin = true,
+				}
+			end,
+		},
+		tex = {
+			function ()
+				return {
+					exe = 'tex-fmt',
+					args = { '-t 4', vim.api.nvim_buf_get_name (0) },
+					stdin = true,
+				}
+			end,
+		},
+		lua = {
+			function ()
+				return {
+					exe = 'stylua',
+					args = {
+						'--config-path '
+							.. vim.fn.stdpath ('config')
+							.. '/styles/stylua.toml',
+						vim.api.nvim_buf_get_name (0),
+					},
+					stdin = false,
+				}
+			end,
+		},
+	},
+	logging = false,
 })
--- after/plugin/lint.lua
+++ after/plugin/lint.lua
@@ -1,30 +1,30 @@
-require('mason-tool-installer').setup({
-    ensure_installed = {
-        'sonarlint-language-server',
-        'prettier',
-        'clang-format',
---        'luaformatter',
-        'autopep8',
-    }
+require ('mason-tool-installer').setup ({
+	ensure_installed = {
+		'sonarlint-language-server',
+		'prettier',
+		'clang-format',
+		'autopep8',
+		'tex-fmt',
+	},
 })
 
-require('sonarlint').setup({
-    server = {
-        cmd = {
-            'sonarlint-language-server',
-            -- Ensure that sonarlint-language-server uses stdio channel
-            '-stdio',
-            '-analyzers',
-            -- paths to the analyzers you need, using those for python and java in this example
-            vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarpython.jar"),
-            vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarcfamily.jar"),
-            -- vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjava.jar"),
-        }
-    },
-    filetypes = {
-        -- Tested and working
-        'python',
-        'cpp',
-        -- 'java',
-    }
+require ('sonarlint').setup ({
+	server = {
+		cmd = {
+			'sonarlint-language-server',
+			-- Ensure that sonarlint-language-server uses stdio channel
+			'-stdio',
+			'-analyzers',
+			-- paths to the analyzers you need, using those for python and java in this example
+			vim.fn.expand ('$MASON/share/sonarlint-analyzers/sonarpython.jar'),
+			vim.fn.expand ('$MASON/share/sonarlint-analyzers/sonarcfamily.jar'),
+			-- vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjava.jar"),
+		},
+	},
+	filetypes = {
+		-- Tested and working
+		'python',
+		'cpp',
+		-- 'java',
+	},
 })
